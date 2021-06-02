b082bc4e1deeba35d288e762747dcd22aef27aa9
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-06-01 22:41:27 -0300

    26.1: Exercícios 1 ao 5

--- trybe-exercises/exercises/Desenvolvimento Back-End/Bloco_26/Dia_01/imc.js
+++ trybe-exercises/exercises/Desenvolvimento Back-End/Bloco_26/Dia_01/imc.js
@@ -1,3 +1,26 @@
-const calculaIMC = (peso, altura) => peso * Math.pow(altura, 2);
+const readline = require('readline-sync');
 
-console.log(calculaIMC(2, 1.7));
+const showIMC = () => {
+  const peso = readline.questionFloat('Qual o seu peso? ');
+
+  const altura = readline.questionFloat('Qual a sua altura? ');
+
+  const imc = calculaIMC(peso, altura)
+
+  const situação = situacao(imc)
+
+  console.log("IMC: %s, situação: %s", imc, situação)
+};
+
+const calculaIMC = (peso, altura) => peso / Math.pow(altura, 2);
+
+const situacao = (imc) => {
+  if(imc < 18.5) return "Abaixo do peso (magreza)";
+  if(imc >= 40) return "Obesidade graus III e IV";
+  if(imc >= 18.5 && imc < 25) return "Peso normal";
+  if(imc >= 25 && imc < 30) return "Acima do peso (sobrepeso)";
+  if(imc >= 30 && imc < 35) return "Obesidade grau I";
+  if(imc >= 35 && imc < 40) return "Obesidade grau II";
+}
+
+showIMC();
