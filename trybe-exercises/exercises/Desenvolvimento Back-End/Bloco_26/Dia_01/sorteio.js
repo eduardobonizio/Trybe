327387b3d6293d47a0313b03540beae510c187c5
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-06-01 23:00:29 -0300

    26.1: exercício 7

---
+++ trybe-exercises/exercises/Desenvolvimento Back-End/Bloco_26/Dia_01/sorteio.js
@@ -0,0 +1,23 @@
+const readline = require('readline-sync');
+
+const sorteia = () => {
+  const novoNumero = numeroAleatorio()
+
+  const numeroEscolhido = readline.questionFloat('Qual foi o numero sorteado? ');
+
+  if(numeroEscolhido === novoNumero){
+    console.log("Parabéns, número correto!")
+  }else{
+    console.log("Opa, não foi dessa vez. O número era %s", novoNumero)
+  }
+
+  const jogarMaisUmaVez = readline.question("Deseja tentar outra vez? Y/N ");
+
+  if(jogarMaisUmaVez.toLowerCase() === 'y') return sorteia();
+
+  return;
+};
+
+const numeroAleatorio = () => Math.floor(Math.random() * 11);
+
+sorteia();
