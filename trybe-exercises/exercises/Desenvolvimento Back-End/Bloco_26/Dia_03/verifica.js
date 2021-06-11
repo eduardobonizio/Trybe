8b29554ba1f56002f9ad2ba38a0eb929025e04d2
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-06-10 23:24:34 -0300

    Exercises 26.3: Exercicio 2

--- trybe-exercises/exercises/Desenvolvimento Back-End/Bloco_26/Dia_03/verifica.js
+++ trybe-exercises/exercises/Desenvolvimento Back-End/Bloco_26/Dia_03/verifica.js
@@ -1,5 +1,6 @@
 module.exports = {
   verificaNumero: (numero) => {
+    if(!numero) return;
     if(numero > 0) return 'positivo';
     if(numero < 0) return 'negativo';
     if(numero === 0) return 'neutro';
