b7bbaac0c65008f4180b5d3565f3208696f8508a
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-08-08 22:08:49 -0300

    Adiciona rota para filtrar author

--- backend/Arquitetura de software/MySql/index.js
+++ backend/Arquitetura de software/MySql/index.js
@@ -12,6 +12,16 @@ app.get('/authors', async (_req, res) => {
   res.status(200).json(authors);
 });
 
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Not found' });
+
+  res.status(200).json(author);
+});
+
 app.get('/books', async (req, res) => {
   const authorId = req.query.author_id;
   if(authorId){
