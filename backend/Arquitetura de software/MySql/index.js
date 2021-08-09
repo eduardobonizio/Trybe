b8dbd7b4d63582df897568b461220820cd3fb4ce
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-08-08 21:50:15 -0300

    Adiciona rota para busca de livros

--- backend/Arquitetura de software/MySql/index.js
+++ backend/Arquitetura de software/MySql/index.js
@@ -3,11 +3,19 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 const Author = require('./models/Author');
+const Books = require('./models/Books');
+
 
 app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
 
   res.status(200).json(authors);
+});
+
+app.get('/books', async (_req, res) => {
+  const bookList = await Books.getAll();
+
+  res.status(200).json(bookList);
 });
 
 app.listen(PORT, () => console.log(`Ouvindo a porta ${PORT}`))
