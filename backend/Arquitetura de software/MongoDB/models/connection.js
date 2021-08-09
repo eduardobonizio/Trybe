7e17b9e246026aa1248e325359afde8721bc9e8b
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-08-09 00:33:45 -0300

    Adiciona .env

--- backend/Arquitetura de software/MongoDB/models/connection.js
+++ backend/Arquitetura de software/MongoDB/models/connection.js
@@ -5,18 +5,13 @@ const OPTIONS = {
     useUnifiedTopology: true,
 }
 
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
-
 let db = null;
 
 const connection = () => {
     return db
     ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-    .then((conn) => {
-    db = conn.db('model_example');
-    return db;
-    })
+    : MongoClient.connect(process.env.DB_URL, OPTIONS)
+    .then((conn) => conn.db(process.env.DB_NAME))
 };
 
 module.exports = connection;
