b0d0e991a39cfb6fef8fafd6c0360f8145adbfb8
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-08-08 22:44:42 -0300

    Adiciona seed do mongodb e refatora a connection

--- backend/Arquitetura de software/MongoDB/models/connection.js
+++ backend/Arquitetura de software/MongoDB/models/connection.js
@@ -1,9 +1,22 @@
-const mysql = require('mysql2/promise');
+const { MongoClient } = require('mongodb');
 
-const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '232352',
-    database: 'model_example' });
+const OPTIONS = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+
+const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+
+let db = null;
+
+const connection = () => {
+    return db
+    ? Promise.resolve(db)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => {
+    db = conn.db('model_example');
+    return db;
+    })
+};
 
 module.exports = connection;
